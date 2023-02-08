c6766a5b984a36c72b2cd7324c79db5acbf146ca
ObliviouZ <bernaz.10golden97@gmail.com>
2023-02-08 02:33:26 +0000

    prod setup part 1

--- image_analysis/wsgi.py
+++ image_analysis/wsgi.py
@@ -8,9 +8,23 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import time
+import traceback
+import signal
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/var/www/django_image_analysis')
+# adjust the Python version in the line below as needed
+sys.path.append('/var/www/django_image_analysis/venv/lib/python3.8/site-packages')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_analysis.settings')
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception:
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
