85066a86dd2c3e8491e84195a79f0445a9574f69
ObliviouZ <bernaz.10golden97@gmail.com>
2023-03-25 16:44:22 +0000

    update to prod

--- mouseapp/urls.py
+++ mouseapp/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', home, name="home"),
     path('saved', saved_distance),
     path('authenticate/', include('django.contrib.auth.urls')),
     path('authenticate/', include('accounts.urls')),
