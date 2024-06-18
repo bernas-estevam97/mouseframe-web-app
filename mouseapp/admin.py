8e943f33ac4cc3cf9273b188fb7f06bee31fd024
Bernardo Estevam <bernaz10.1997golden@gmail.com>
2024-06-18 12:51:52 +0100

    added import export functionality

--- mouseapp/admin.py
+++ mouseapp/admin.py
@@ -1,5 +1,8 @@
 from .models import *
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
+class ExcelAdmin(ImportExportModelAdmin):
+    pass
 
-admin.site.register(SavedDistances)
+admin.site.register(SavedDistances, ExcelAdmin)
--- mouseapp/settings.py
+++ mouseapp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'mouseapp',
+    'import_export',
     'accounts',
 ]
 
