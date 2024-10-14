221e956e617e5fa70ef4bcc84df80e2506aa62dc
bernas-estevam97 <bernaz.10golden97@gmail.com>
2024-10-14 14:25:06 +0100

    working on color pallettes

--- mouseapp/settings.py
+++ mouseapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = key
 #key = os.environ.get('S_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '94.46.171.187', 'mouseframe.pt']
